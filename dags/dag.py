eecbb5e6fb20c545c55e16d3ddde5ae3ed589c4c
Palina Burdina <93486995+cipauline@users.noreply.github.com>
2025-02-14 03:29:58 +0300

    Update dag.py

--- dags/dag.py
+++ dags/dag.py
@@ -1,90 +1,76 @@
-from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from src.extract import fetch_data
-from src.transform import clean_data
-from src.load import load_to_db
-import yaml
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from sqlalchemy import create_engine
 import pandas as pd
-import logging
-
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
 
 
-def load_config():
-    with open("config/config.yaml", "r") as file:
-        return yaml.safe_load(file)
+def extract_data(url, temp_file, **context):
+    """
+    Extract data from a URL and save it as a JSON file.
+    """
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+    with open(temp_file, 'w') as f:
+        json.dump(data, f)
 
 
-def extract_data(**kwargs):
-    try:
-        config = load_config()
-        api_url = config["api"]["url"]
-        data = fetch_data(api_url)
-        kwargs["ti"].xcom_push(key="data", value=data)
-    except Exception as e:
-        logging.error(f"Error extracting data: {str(e)}")
-        raise
+def transform_data(temp_file, temp_file_agg, **context):
+    """
+    Extracting only neccesery columns, transforming datatypes, adding columns
+    """
+    df = pd.DataFrame(temp_file)
+    df = df[['date_utc', 'success', 'rocket', 'launchpad']]
+    df['date_utc'] = pd.to_datetime(df['date_utc'])
+    df['year'] = df['date_utc'].dt.year
+    df['month'] = df['date_utc'].dt.month
+    df['hour'] = df['date_utc'].dt.hour
+    df.reset_index().to_csv(temp_file_agg) 
+    
 
-
-def transform_data(**kwargs):
-    try:
-        ti = kwargs["ti"]
-        raw_data = ti.xcom_pull(key="data")
-        df = clean_data(raw_data)
-        kwargs["ti"].xcom_push(key="df", value=df.to_dict(orient="records"))
-    except Exception as e:
-        logging.error(f"Error transforming data: {str(e)}")
-        raise
-
-
-def load_data(**kwargs):
-    try:
-        config = load_config()
-        ti = kwargs["ti"]
-        df_records = ti.xcom_pull(key="df")
-        df = pd.DataFrame.from_records(df_records)
-        db_connection = f"postgresql+psycopg2://{config['database']['user']}:{config['database']['password']}@{config['database']['host']}:{config['database']['port']}/{config['database']['dbname']}"
-        load_to_db(df, db_connection, config["database"]["table_name"])
-    except Exception as e:
-        logging.error(f"Error loading data to DB: {str(e)}")
-        raise
-
-
-default_args = {
-    "owner": "airflow",
-    "retries": 3,
-    "retry_delay": timedelta(minutes=5),
-    "start_date": datetime(2025, 2, 10),
-}
+def load_data(temp_file_agg, **context):
+    """
+    Loading data into Postgres Database
+    """
+    engine = create_engine('postgresql+psycopg2://airflow:airflow@localhost:5432/spacex_db')
+    temp_file_agg.to_sql('test_table', con=engine, if_exists='append', index=False)
 
 
 dag = DAG(
-    "spacex_etl",
-    default_args=default_args,
-    description="ETL pipeline for SpaceX launches data",
-    schedule_interval=timedelta(days=7),
-    catchup=False,
+    dag_id='dag',
+    default_args={'owner': 'airflow'},
+    schedule_interval='@monthly',
+    start_date=days_ago(1)
 )
 
 
 extract_task = PythonOperator(
     task_id="extract_data",
     python_callable=extract_data,
+    op_kwargs={
+            'url': 'https://api.spacexdata.com/v4/launches',
+            'tmp_file': '/tmp/file.csv'
+    },
     dag=dag,
 )
 
 transform_task = PythonOperator(
     task_id="transform_data",
     python_callable=transform_data,
+    op_kwargs={
+            'tmp_file': '/tmp/file.csv',
+            'tmp_agg_file': '/tmp/file_agg.csv'
+    },
     dag=dag,
 )
 
 load_task = PythonOperator(
     task_id="load_data",
     python_callable=load_data,
+    op_kwargs={
+            'tmp_agg_file': '/tmp/file_agg.csv'
+    },
     dag=dag,
 )
 
